74726b3246e2120c99efa536c8d7c3a1784d779a
rot13maxi <rot13maxi@protonmail.com>
2022-12-29 21:31:32 -0500

    contact loading DONE! messy, but works

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::io::Write;
 use std::path::PathBuf;
 use std::{env, fs};
 use std::borrow::Borrow;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::str::FromStr;
 use nostr_rust::Identity;
 use nostr_rust::nostr_client::Client;
@@ -13,12 +13,10 @@ use nostr_rust::req::ReqFilter;
 use toml::{toml, Value};
 
 #[derive(Deserialize, Serialize)]
-struct Contact {
-    nip05: Option<String>,
-    pubkey: String,
+struct ContactList {
+    nip05_contacts: HashMap<String, String>, // nip05 -> pubkey
+    unwashed_masses: HashSet<String>, // pubkey
 }
-
-type ContactList = Vec<Contact>;
 
 #[derive(Deserialize, Serialize)]
 struct Config {
@@ -33,7 +31,10 @@ impl Default for Config {
         Self {
             privkey: "YOUR_HEX_ENCODED_PRIVKEY".to_string(),
             relays: vec!["wss://relay.damus.io".to_string()],
-            contact_list: Vec::new(),
+            contact_list: ContactList {
+                nip05_contacts: HashMap::new(),
+                unwashed_masses: HashSet::new(),
+            },
         }
     }
 }
@@ -62,7 +63,7 @@ fn main() {
     let config_path = cli.config.unwrap_or_else(|| {
         let mut p = env::home_dir().unwrap();
         p.push(".smart-follow");
-        p.push("config.toml");
+        p.push("config.json");
         p
     });
 
@@ -71,23 +72,21 @@ fn main() {
             let home_dir = env::home_dir().expect("Failed to get home directory");
             let new_dir = home_dir.join(".smart-follow");
             fs::create_dir(&new_dir);
-            let fname = new_dir.join("config.toml");
+            let fname = new_dir.join("config.json");
             let mut file = fs::File::create(&fname).expect(&format!(
                 "Could not create config file at {}",
                 fname.to_str().unwrap()
             ));
-            file.write_all(toml::to_string(&Config::default()).unwrap().as_bytes())
+            println!("{}", serde_json::to_string(&Config::default()).unwrap());
+            file.write_all(serde_json::to_string(&Config::default()).unwrap().as_bytes())
                 .expect("Could not write config file");
             println!("Empty config file written to {}", fname.to_str().unwrap());
         }
         Commands::LoadFollows => {
-            // TODO: this will overwrite the contact list in the config. make that an option.
-            let mut config: Config = toml::from_str(&fs::read_to_string(&config_path).expect(
+            let mut config: Config = serde_json::from_str(&fs::read_to_string(&config_path).expect(
                 "Could not open config file. check your path or run the `gen-config` command",
             ))
-            .expect("Could not parse config file.");
-
-            config.contact_list = Vec::new();
+                .expect("Could not parse config file.");
 
             let identity = Identity::from_str(&config.privkey).expect("Could not load secret key from config. make sure it's hex-encoded");
             let relays: Vec<&str> = config.relays.iter().map(|relay| relay.borrow()).collect();
@@ -105,31 +104,56 @@ fn main() {
                 limit: None,
             }]).expect("Could not fetch kind-3 events from relays");
 
-            let mut contact_pubkeys = HashSet::new();
+            // TODO: this will overwrite the contact list in the config. make that an option.
+            config.contact_list.unwashed_masses = HashSet::new();
             for cl in contact_lists {
                 for tag in cl.tags {
                     if tag.first().unwrap() == "p" {
                         if let Some(k) = tag.get(1) {
-                            contact_pubkeys.insert(k.to_string());
+                            config.contact_list.unwashed_masses.insert(k.to_string());
                         }
-                    } else {continue;}
+                    } else { continue; }
                 }
             }
 
-            println!("Got an aggregate contact list of {} follows. Writing to config file.", contact_pubkeys.len());
-            for pubkey in contact_pubkeys {
-                let c = Contact {
-                    pubkey,
-                    nip05: None,
-                };
-                config.contact_list.push(c);
+            println!("Got an aggregate contact list of {} follows. Writing to config file.", config.contact_list.unwashed_masses.len());
+            {
+                let mut f = fs::File::create(&config_path).expect("could not open config file for writing");
+                f.write_all(serde_json::to_string(&config).unwrap().as_bytes()).expect("Could not write config to file");
             }
-            let mut f = fs::File::create(&config_path).expect("could not open config file for writing");
-            f.write_all(toml::to_string(&config).unwrap().as_bytes()).expect("Could not write config to file");
+            println!("Going to try resolving contacts to NIP05 identifiers...");
 
-            println!("Going to try resolving contacts to NIP05 identifiers...")
-            for mut contact in config.contact_list {
-
+            // TODO: this will overwrite the contact list in the config. make that an option.
+            config.contact_list.nip05_contacts = HashMap::new();
+            let metadata_events = client.get_events_of(vec![ReqFilter {
+                ids: None,
+                authors: Some(config.contact_list.unwashed_masses.clone().into_iter().collect()),
+                kinds: Some(vec![0]),
+                e: None,
+                p: None,
+                since: None,
+                until: None,
+                limit: None,
+            }]).expect("Could not fetch kind-0 (metadata) events from relays");
+            let mut latest_ts: HashMap<String, u64> = HashMap::new();
+            for event in metadata_events {
+                if let Ok(content) = serde_json::from_str::<serde_json::Value>(&event.content) {
+                    if let Some(content) = content.as_object() {
+                        if let Some(nip05) = content.get("nip05") {
+                            let latest_update = latest_ts.get(&event.pub_key).unwrap_or(&0);
+                            if event.created_at > *latest_update {
+                                latest_ts.insert(event.pub_key.clone(), event.created_at);
+                                config.contact_list.nip05_contacts.insert(nip05.as_str().unwrap().to_string(), event.pub_key.clone());
+                                config.contact_list.unwashed_masses.remove(&event.pub_key);
+                            }
+                        }
+                    }
+                }
+            }
+            println!("Managed to find NIP05 identifiers for {} entries! Going to write to disk", config.contact_list.nip05_contacts.len());
+            {
+                let mut f = fs::File::create(&config_path).expect("could not open config file for writing");
+                f.write_all(serde_json::to_string(&config).unwrap().as_bytes()).expect("Could not write config to file");
             }
 
         }
